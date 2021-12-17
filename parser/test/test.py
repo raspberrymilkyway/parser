e939cfcc0b0056f7e59b53dc32b728e7e97f6191
Kylee <23221736+raspberrymilkyway@users.noreply.github.com>
2021-12-16 21:31:06 -0600

    Delete parser directory
    (to make less redundant and confusing)

--- parser/test/test.py
+++
@@ -1,9 +0,0 @@
-print("Charmender did "+ str(charmender_attack)+" damage")
-
-eq1 = 2 * -5 + 20
-while charmender_HP > 0 and squirtle_HP > 0:
-if turn == 1:
-if(eq1 != 0):
-        for i in range(2, int(num/2)+2):
-name = "Ash Ketchum"
-name += "Ash Ketchum"
